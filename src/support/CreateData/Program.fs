d127e7c0485ff72048b1a72e07cd8a6254faf6a3
Richard Cox <richard@rjcox.co.uk>
2024-01-18 11:19:47 +0000

    Create data creation project

---
+++ src/support/CreateData/Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+
+[<EntryPoint>]
+let main(args) =
+    printfn $"iBRC CreateData"
+    printfn $"Working folder: {Environment.CurrentDirectory}"
+    printfn ""
+
+    if args.Length <> 1 then
+        eprintfn "Invalid command line"
+        eprintfn "Usage:"
+        eprintfn "  CreateData <count>"
+        1
+    else
+        let count = Int32.Parse(args[0])
+        printfn $"Creating {count} rows"
+        0
