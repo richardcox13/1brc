9c92224f447da32c29a1f156a2b44ea1369fd1fc
Richard Cox <richard@rjcox.co.uk>
2024-01-23 14:14:21 +0000

    Track GC counts before exit

--- src/1brc/Simple/Program.fs
+++ src/1brc/Simple/Program.fs
@@ -21,4 +21,6 @@ let main(args) =
         let rows = SimpleProcessor.run filename
         sw.Stop()
         printfn $"Complete with {rows} records in {sw.Elapsed:``h':'mm':'ss'.'fff``}."
+        printfn $"GC counts 0: {GC.CollectionCount(0)}; 1: {GC.CollectionCount(1)}; 2: {GC.CollectionCount(2)}; "
+
         0
