a2618112783b4856d1f57b929fcd8f6eb12bb627
Richard Cox <richard@rjcox.co.uk>
2024-01-23 13:50:48 +0000

    Start to process content into dictionary

--- src/1brc/Simple/Program.fs
+++ src/1brc/Simple/Program.fs
@@ -1,4 +1,6 @@
 ﻿open System
+open System.Diagnostics
+open OmeBillion.Simple
 
 [<EntryPoint>]
 let main(args) =
@@ -6,5 +8,17 @@ let main(args) =
     printfn $"Working folder: {Environment.CurrentDirectory}"
     printfn ""
 
+    if args.Length <> 1 then
+        eprintfn "Invalid command line"
+        eprintfn "Usage"
+        eprintfn "  Simple <data-file"
+        1
+    else
+        let filename = args[0]
+        printfn $"Reading from \"{filename}\""
 
-    0
+        let sw = Stopwatch.StartNew()
+        let rows = SimpleProcessor.run filename
+        sw.Stop()
+        printfn $"Complete with {rows} records in {sw.Elapsed:``h':'mm':'ss'.'fff``}."
+        0
---
+++ src/1brc/Simple/SimpleProcessor.fs
@@ -0,0 +1,44 @@
+﻿module OmeBillion.Simple.SimpleProcessor
+
+open System
+open System.Collections.Generic
+open System.IO
+
+[<Struct>]
+type InputRecord =
+    {
+        City: string
+        Value: float
+    }
+
+
+type Accumulator =
+    {
+        City: string
+        Count: int
+        Sum: float
+    }
+
+let parseOneRecord (record: string) =
+    let fs = record.Split(';')
+    assert (fs.Length = 2)
+    let city = fs[0]
+    let value = Double.Parse(fs[1])
+    { City = city; Value = value}
+
+let run filename =
+
+    let records
+        = File.ReadLines(filename)
+          |> Seq.map (fun r -> parseOneRecord r)
+
+    let mutable count = 0
+    let data = new Dictionary<string, Accumulator>()
+    for r in records do
+        data[r.City] <- match data.TryGetValue(r.City) with
+                        | (false, _) -> { City = r.City; Count = 1; Sum = r.Value }
+                        | (true, prev) -> { prev with Count = prev.Count+1; Sum = prev.Sum + r.Value }
+
+        count <- count+1
+
+    count
